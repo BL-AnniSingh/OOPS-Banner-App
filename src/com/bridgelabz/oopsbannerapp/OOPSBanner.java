2467c4a80c8ab5fa5fa3a1e9443dfe3a424c69f9
BL-AnniSingh <anni.singh@bridgelabz.com>
2026-01-31 14:52:59 +0530

    Rendering oops banner using static method

--- src/com/bridgelabz/oopsbannerapp/OOPSBanner.java
+++ src/com/bridgelabz/oopsbannerapp/OOPSBanner.java
@@ -1,62 +1,60 @@
 package com.bridgelabz.oopsbannerapp;
 
 public class OOPSBanner {
+
+    // Pattern for letter 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+                "   ******   ",
+                " **      ** ",
+                "**        **",
+                "**        **",
+                "**        **",
+                " **      ** ",
+                "   ******   "
+        };
+    }
+
+    // Pattern for letter 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+                "********    ",
+                "**      **  ",
+                "**      **  ",
+                "********    ",
+                "**          ",
+                "**          ",
+                "**          "
+        };
+    }
+
+    // Pattern for letter 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+                "   ******   ",
+                " **         ",
+                "**          ",
+                "   ******   ",
+                "        **  ",
+                "        **  ",
+                "   ******   "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] lines = new String[7];
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        lines[0] = String.join("  ",
-                " ******  ",   // O
-                " ******  ",   // O
-                "******** ",   // P
-                " ******  "    // S
-        );
-
-        lines[1] = String.join("  ",
-                "**    ** ",
-                "**    ** ",
-                "**    ** ",
-                "**       "
-        );
-
-        lines[2] = String.join("  ",
-                "**    ** ",
-                "**    ** ",
-                "**    ** ",
-                "**       "
-        );
-
-        lines[3] = String.join("  ",
-                "**    ** ",
-                "**    ** ",
-                "******** ",
-                " ******  "
-        );
-
-        lines[4] = String.join("  ",
-                "**    ** ",
-                "**    ** ",
-                "**       ",
-                "      ** "
-        );
-
-        lines[5] = String.join("  ",
-                "**    ** ",
-                "**    ** ",
-                "**       ",
-                "      ** "
-        );
-
-        lines[6] = String.join("  ",
-                " ******  ",
-                " ******  ",
-                "**       ",
-                " ******  "
-        );
-
-        // Print banner
-        for (String line : lines) {
-            System.out.println(line);
+        // Print "OOPS"
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                    oPattern[i] + "  " +   // O
+                            oPattern[i] + "  " +   // O
+                            pPattern[i] + "  " +   // P
+                            sPattern[i]            // S
+            );
         }
     }
 }
